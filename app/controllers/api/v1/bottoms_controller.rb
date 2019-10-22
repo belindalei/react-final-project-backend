3efa12dab387a204ceb1279fb7f6d5083f7762d0
Belinda Lei <belinda.lei95@gmail.com>
2019-10-22 13:59:17 -0700

    added create top and bottom

--- app/controllers/api/v1/bottoms_controller.rb
+++ app/controllers/api/v1/bottoms_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::BottomsController < ApplicationController
     @bottoms = Bottom.all 
     render json: @bottoms
   end
+
+  def create
+    bottom = Bottom.new(bottom_params)
+
+    if bottom.save
+    render json: bottom
+    else 
+    render json: {errors: bottom.errors.full_messages}
+    end
+  end
+
+  def bottom_params
+    params.require(:bottom).permit(:name, :img_url, :category, :color)
+  end
+
 end
--- app/controllers/api/v1/tops_controller.rb
+++ app/controllers/api/v1/tops_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::TopsController < ApplicationController
     @tops = Top.all 
     render json: @tops
   end
+
+  def create
+    top = Top.new(top_params)
+
+    if top.save
+    render json: top
+    else 
+    render json: {errors: top.errors.full_messages}
+    end
+  end
+
+  def top_params
+    params.require(:top).permit(:name, :img_url, :category, :color)
+  end
+
 end
