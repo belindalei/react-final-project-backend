6be143e44d011453cf6168f72ecd727f84a94c6a
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-10-22 09:46:53 -0400

    added notes for possible validations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
-    def index
-        @users = User.all
-        render json: @users
-    end
+  def show
+      @user = User.find(params[:id])
+      #should we serialize the user to send the outfits?
+      render json: @user
+  end
 
-    def show
-        @user = User.find(params[:id])
-        #should we serialize the user to send the outfits?
-        render json: @user
-    end
+  def create 
+    user = User.create(user_params)
+    # check if valid once we have validations -- how to handle validations in react? Alert window? Try/catch?
+    token = JWT.encode({user: user.id}, "clueless")
+
+    render json: {user: user, token: token}
+  end
+  
+    # def index
+    #     @users = User.all
+    #     render json: @users
+    # end
 
     # def create
     #     user = User.new(user_params)
@@ -22,21 +30,14 @@ class Api::V1::UsersController < ApplicationController
 
     # end
 
-    def create 
-      user = User.create(user_params)
-      # check if valid once we have validations
-      token = JWT.encode({user: user.id}, "clueless")
-
-      render json: {user: user, token: token}
-    end
 
 
-    def destroy
-        user = User.find(params[:id])
-        user.destroy
+    # def destroy
+    #     user = User.find(params[:id])
+    #     user.destroy
 
-        render json: {message: "Successfully deleted"}
-    end
+    #     render json: {message: "Successfully deleted"}
+    # end
 
     private
 
